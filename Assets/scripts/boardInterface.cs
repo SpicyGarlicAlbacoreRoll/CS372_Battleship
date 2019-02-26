77f3b715dec0adceeb4ffaf6023d40074c77b5b0
Dylan Palmieri <dgpalmieri@gmail.com>
2019-02-25 15:23:52 -0900

    Modify boardInterface adding psudocode

--- Assets/scripts/boardInterface.cs
+++ Assets/scripts/boardInterface.cs
@@ -7,6 +7,7 @@ public class boardInterface : MonoBehaviour
 
     public shipClass ship;
     //public playerClass player; maybe we need this?
+    public Grid Grid;
 
     // Start is called before the first frame update
     void Start()
@@ -26,21 +27,26 @@ public class boardInterface : MonoBehaviour
     void getMouseClick()
     {
         RaycastHit clickInfo = new RaycastHit();
+
+
         bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out clickInfo);
-        if (hit)
+        if (hit) //  && clickInfo.transform.gameObject.tag != "AlreadyShot" to differentiate between shot and not shot?
         {
             if (clickInfo.transform.gameObject.tag == "Ship") // && clickInfo.transform.gameObject.tag == player.tag (if we can differentiate between players in a class)
             {
                 ship.takeDamage();
-                markBoard();
+                markBoard(clickInfo);
             }
         }
     }
 
     // markBoard
     // after firing, marks the board as either a hit or miss
-    void markBoard()
+    void markBoard(RaycastHit raycast)
     {
+        Vector3Int cellPosition = Grid.WorldToCell(raycast.point);
+        // tell the UI to write a hit or miss marker here
+        // add cell to list of hit cells?
 
     }
 
